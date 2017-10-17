1d5d4a27c309466d17977c11dc827aaf88eb7f3e
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-17 00:52:02 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,7 @@ class Song
   def self.find_or_create_by_name(name)
     if self.find_by_name(name) == true then return self
     else create_by_name(name) end
-      return self.song
+      return self.name
   end
 
 end
