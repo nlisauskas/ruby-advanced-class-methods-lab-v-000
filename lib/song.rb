eb38458ebd780e3d0b3923d897bee8b3f60548ab
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-17 01:09:41 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,4 +43,10 @@ class Song
     @@all.sort_by!{|song| song.name}
   end
 
+  def self.new_from_filename(filename)
+    song = self.new
+    song.name = filename.split('-')[1].strip
+    song.artist_name = filename.split('-')[0].strip
+  end
+
 end
