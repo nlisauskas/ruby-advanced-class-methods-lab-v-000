14886d2fea83a7666bcb04808118441b82c89b6e
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-17 01:12:35 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,7 +45,7 @@ class Song
 
   def self.new_from_filename(filename)
     song = self.new
-    song.name = filename.split('-')[1].strip
+    song.name = filename.split('-')[1].strip.chomp(".mp3")
     song.artist_name = filename.split('-')[0].strip
     song
   end
